04287a0c0c96454d455441927691c2a5ff81b502
mic-meier <mic.l.meier@gmail.com>
2019-11-14 18:53:38 +0100

    Solved chapter 3 exercise recursion

--- 3_functions/functions.md
+++ 3_functions/functions.md
@@ -9,3 +9,7 @@
 Write a function that takes two arguments and returns their minimum.
 
 ---
+
+[Recursion](./recursion.js)
+
+Write a recursive function that accepts a single integer as its parameter and return true if the integer is even, and false if it's not.
---
+++ 3_functions/recursion.js
@@ -0,0 +1,15 @@
+function isEven(int) {
+    let absInt = Math.abs(int);
+    if (absInt === 0) {
+        return true;
+    } else if (absInt === 1) {
+        return false;
+    } else {
+        return isEven(absInt - 2);
+    }
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
+console.log(isEven(-2));
